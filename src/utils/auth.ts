40ce43f4d863baa0f2eaa262b3537d2d8275f141
mharrymh <149264867+mharrymh@users.noreply.github.com>
2025-11-07 00:35:20 -0500

    update docker configs. create roles enum

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,8 +1,5 @@
 import { jwtVerify } from "jose";
-import type { Role } from "@/utils/access";
-
-const ALL_ROLES: Role[] = ["student","admin","teacher","subjectleader","examiner"];
-const isRole = (x: unknown): x is Role => typeof x === "string" && ALL_ROLES.includes(x as Role);
+import { isRole, type Role } from "@/utils/access";
 
 export async function getRolesFromToken(token?: string): Promise<Role[]> {
   if (!token) return [];
